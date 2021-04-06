f71c9e5511d532a62d6913bd0b6c7a80f3f5f852
hopwesley <ws@sinoc.io>
2021-04-06 13:50:57 +0800

    remove arm for android

--- bls/bls.go
+++ bls/bls.go
@@ -5,7 +5,7 @@ package bls
 #cgo LDFLAGS:-lbls384_256 -lstdc++ -lm
 #cgo ios LDFLAGS:-L${SRCDIR}/lib/ios
 #cgo android,arm64 LDFLAGS:-L${SRCDIR}/lib/linux/arm64
-//#cgo arm LDFLAGS:-L${SRCDIR}/lib/android/armeabi-v7a
+#cgo arm LDFLAGS:-L${SRCDIR}/lib/android/armeabi-v7a
 #cgo android,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
 #cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
 #cgo linux,arm64 LDFLAGS:-L${SRCDIR}/lib/linux/arm64
